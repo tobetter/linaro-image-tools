866222fb14471d38b0ff2c13676d9321697d60e1
James Westby <james.westby@canonical.com>
2010-08-30 20:43:29 -0400

    Factor out some common code in the tests.

--- hwpack/tests/test_better_tarfile.py
+++ hwpack/tests/test_better_tarfile.py
@@ -15,61 +15,53 @@ class TarFileTests(TestCase):
         with standard_tarfile(backing_file, seek=False) as tf:
             self.assertEqual([], tf.getnames())
 
-    def test_create_file_from_string_adds_path(self):
+    def create_simple_tarball_with_file(self, **kwargs):
         backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
+        with writeable_tarfile(backing_file, **kwargs) as tf:
             tf.create_file_from_string("foo", "bar")
+        return backing_file
+
+    def test_create_file_from_string_adds_path(self):
+        backing_file = self.create_simple_tarball_with_file()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(["foo"], tf.getnames())
 
     def test_create_file_from_string_uses_content(self):
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
-            tf.create_file_from_string("foo", "bar")
+        backing_file = self.create_simple_tarball_with_file()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual("bar", tf.extractfile("foo").read())
 
     def test_create_file_from_string_sets_size(self):
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
-            tf.create_file_from_string("foo", "bar")
+        backing_file = self.create_simple_tarball_with_file()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(3, tf.getmember("foo").size)
 
     def test_create_file_from_string_sets_mode(self):
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
-            tf.create_file_from_string("foo", "bar")
+        backing_file = self.create_simple_tarball_with_file()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(0644, tf.getmember("foo").mode)
 
     def test_create_file_from_string_sets_type(self):
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
-            tf.create_file_from_string("foo", "bar")
+        backing_file = self.create_simple_tarball_with_file()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(tarfile.REGTYPE, tf.getmember("foo").type)
 
     def test_create_file_from_string_sets_linkname(self):
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
-            tf.create_file_from_string("foo", "bar")
+        backing_file = self.create_simple_tarball_with_file()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual('', tf.getmember("foo").linkname)
 
     def test_create_file_uses_default_mtime(self):
         now = 126793
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file, default_mtime=now) as tf:
-            tf.create_file_from_string("foo", "bar")
+        backing_file = self.create_simple_tarball_with_file(
+            default_mtime=now)
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(now, tf.getmember("foo").mtime)
 
     def test_create_file_uses_default_uid(self):
         uid = 1259
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file, default_uid=uid) as tf:
-            tf.create_file_from_string("foo", "bar")
+        backing_file = self.create_simple_tarball_with_file(
+            default_uid=uid)
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(uid, tf.getmember("foo").uid)
 
@@ -83,98 +75,85 @@ class TarFileTests(TestCase):
 
     def test_create_file_uses_default_uname(self):
         uname = "someperson"
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file, default_uname=uname) as tf:
-            tf.create_file_from_string("foo", "bar")
+        backing_file = self.create_simple_tarball_with_file(
+            default_uname=uname)
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(uname, tf.getmember("foo").uname)
 
     def test_create_file_uses_default_gname(self):
         gname = "somegroup"
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file, default_gname=gname) as tf:
-            tf.create_file_from_string("foo", "bar")
+        backing_file = self.create_simple_tarball_with_file(
+            default_gname=gname)
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(gname, tf.getmember("foo").gname)
 
-    def test_create_dir_adds_path(self):
+    def create_simple_tarball_with_dir(self, **kwargs):
         backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
+        with writeable_tarfile(backing_file, **kwargs) as tf:
             tf.create_dir("foo")
+        return backing_file
+
+    def test_create_dir_adds_path(self):
+        backing_file = self.create_simple_tarball_with_dir()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(["foo"], tf.getnames())
 
     def test_create_dir_sets_name(self):
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
-            tf.create_dir("foo")
+        backing_file = self.create_simple_tarball_with_dir()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual("foo", tf.getmember("foo").name)
 
     def test_create_dir_sets_type(self):
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
-            tf.create_dir("foo")
+        backing_file = self.create_simple_tarball_with_dir()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(tarfile.DIRTYPE, tf.getmember("foo").type)
 
     def test_create_dir_sets_size(self):
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
-            tf.create_dir("foo")
+        backing_file = self.create_simple_tarball_with_dir()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(0, tf.getmember("foo").size)
 
     def test_create_dir_sets_mode(self):
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
-            tf.create_dir("foo")
+        backing_file = self.create_simple_tarball_with_dir()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(0755, tf.getmember("foo").mode)
 
     def test_create_dir_sets_linkname(self):
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file) as tf:
-            tf.create_dir("foo")
+        backing_file = self.create_simple_tarball_with_dir()
         with standard_tarfile(backing_file) as tf:
             self.assertEqual('', tf.getmember("foo").linkname)
 
     def test_create_dir_uses_default_mtime(self):
         now = 126793
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file, default_mtime=now) as tf:
-            tf.create_dir("foo")
+        backing_file = self.create_simple_tarball_with_dir(
+            default_mtime=now)
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(now, tf.getmember("foo").mtime)
 
     def test_create_dir_uses_default_uid(self):
         uid = 1259
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file, default_uid=uid) as tf:
-            tf.create_dir("foo")
+        backing_file = self.create_simple_tarball_with_dir(
+            default_uid=uid)
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(uid, tf.getmember("foo").uid)
 
     def test_create_dir_uses_default_gid(self):
         gid = 2259
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file, default_gid=gid) as tf:
-            tf.create_dir("foo")
+        backing_file = self.create_simple_tarball_with_dir(
+            default_gid=gid)
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(gid, tf.getmember("foo").gid)
 
     def test_create_dir_uses_default_uname(self):
         uname = "someperson"
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file, default_uname=uname) as tf:
-            tf.create_dir("foo")
+        backing_file = self.create_simple_tarball_with_dir(
+            default_uname=uname)
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(uname, tf.getmember("foo").uname)
 
     def test_create_dir_uses_default_gname(self):
         gname = "somegroup"
-        backing_file = StringIO()
-        with writeable_tarfile(backing_file, default_gname=gname) as tf:
-            tf.create_dir("foo")
+        backing_file = self.create_simple_tarball_with_dir(
+            default_gname=gname)
         with standard_tarfile(backing_file) as tf:
             self.assertEqual(gname, tf.getmember("foo").gname)
