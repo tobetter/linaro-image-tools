dd60fee8255f3d024f60f97059cfdd28c8ee864c
James Westby <james.westby@canonical.com>
2010-08-30 20:28:04 -0400

    Add some documentation.

--- hwpack/tarfile_matchers.py
+++ hwpack/tarfile_matchers.py
@@ -2,8 +2,15 @@ from testtools.matchers import Matcher, Mismatch
 
 
 class TarfileMissingPathMismatch(Mismatch):
+    """A Mismatch indicating that a required path was missing from a tarfile.
+    """
 
     def __init__(self, tarball, path):
+        """Create a TarfileMissingPathMismatch Mismatch.
+
+        :param tarball: the tarfile that was checked.
+        :param path: the path that was expected to be present.
+        """
         self.tarball = tarball
         self.path = path
 
@@ -12,8 +19,19 @@ class TarfileMissingPathMismatch(Mismatch):
 
 
 class TarfileWrongValueMismatch(Mismatch):
+    """A Mismatch indicating that an entry in a tarfile was not as expected.
+    """
 
     def __init__(self, attribute, tarball, path, expected, actual):
+        """Create a TarfileWrongValueMismatch Mismatch.
+
+        :param attribute: the attribute that was not as expected.
+        :type attribute: str
+        :param tarball: the tarfile that was checked.
+        :param path: the path that was checked.
+        :param expected: the expected value of the attribute.
+        :param actual: the value that was found.
+        """
         self.attribute = attribute
         self.tarball = tarball
         self.path = path
@@ -27,10 +45,36 @@ class TarfileWrongValueMismatch(Mismatch):
 
 
 class TarfileHasFile(Matcher):
+    """Check that a tarfile has an entry with certain values."""
 
     def __init__(self, path, type=None, size=None, mtime=None, mode=None,
                  linkname=None, uid=None, gid=None, uname=None, gname=None,
                  content=None):
+        """Create a TarfileHasFile Matcher.
+
+        :param path: the path that must be present.
+        :type path: str
+        :param type: the type of TarInfo that must be at `path`, or None
+            to not check.
+        :param size: the size that the entry at `path` must have, or None
+            to not check.
+        :param mtime: the mtime that the entry at `path` must have, or None
+            to not check.
+        :param mode: the mode that the entry at `path` must have, or None
+            to not check.
+        :param linkname: the linkname that the entry at `path` must have,
+            or None to not check.
+        :param uid: the user id that the entry at `path` must have, or None
+            to not check.
+        :param gid: the group id that the entry at `path` must have, or None
+            to not check.
+        :param uname: the username that the entry at `path` must have, or
+            None to not check.
+        :param gname: the group name that the entry at `path` must have, or
+            None to not check.
+        :param content: the content that `path` must have when extracted,
+            or None to not check.
+        """
         self.path = path
         self.type = type
         self.size = size
@@ -44,6 +88,7 @@ class TarfileHasFile(Matcher):
         self.content = content
 
     def match(self, tarball):
+        """Match a tarfile.TarFile against the expected values."""
         if self.path not in tarball.getnames():
             return TarfileMissingPathMismatch(tarball, self.path)
         info = tarball.getmember(self.path)
