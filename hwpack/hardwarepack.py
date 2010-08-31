767ce536fee1f577fff3086097b9d20d1b5b314d
James Westby <james.westby@canonical.com>
2010-08-31 14:54:57 -0400

    Add docstrings.

--- hwpack/hardwarepack.py
+++ hwpack/hardwarepack.py
@@ -2,9 +2,35 @@ from hwpack.better_tarfile import writeable_tarfile
 
 
 class Metadata(object):
+    """Metadata for a hardware pack.
+
+    This metadata is what is required and optional for the metadata file
+    that ends up in the hardware pack.
+
+    In addition str() can be used to create the contents of the metdata
+    file for the hardware pack that this metadata file refers to.
+
+    :ivar name: the name of the hardware pack.
+    :type name: str
+    :ivar version: the version of the hardware pack.
+    :type version: str
+    :ivar origin: the origin of the hardware pack, or None if the origin
+        is not known.
+    :type origin: str or None
+    :ivar maintainer: the maintainer of the hardware pack, or None if
+        not known.
+    :type maintainer: str or None
+    :ivar support: the support status of this hardware pack, or None if
+         not know.
+    :type support: str or None
+    """
 
     def __init__(self, name, version, origin=None, maintainer=None,
                  support=None):
+        """Create the Metadata for a hardware pack.
+
+        See the instance variables for a description of the arguments.
+        """
         self.name = name
         self.version = version
         self.origin = origin
@@ -12,6 +38,7 @@ class Metadata(object):
         self.support = support
 
     def __str__(self):
+        """Get the contents of the metadata file."""
         metadata = "NAME=%s\n" % self.name
         metadata += "VERSION=%s\n" % self.version
         if self.origin is not None:
@@ -24,6 +51,13 @@ class Metadata(object):
 
 
 class HardwarePack(object):
+    """The representation of a hardware pack.
+
+    :ivar metadata: the metadata of this hardware pack.
+    :type metadata: Metadata
+    :ivar FORMAT: the format of hardware pack that should be created.
+    :type FORMAT: str
+    """
 
     FORMAT = "1.0"
     FORMAT_FILENAME = "FORMAT"
@@ -35,9 +69,22 @@ class HardwarePack(object):
     SOURCES_LIST_GPG_DIRNAME = "sources.list.d.gpg"
 
     def __init__(self, metadata):
+        """Create a HardwarePack.
+
+        :param metadata: the metadata to use.
+        :type metadata: Metadata
+        """
         self.metadata = metadata
 
     def filename(self):
+        """The filename that this hardware pack should have.
+
+        Returns the filename that the hardware pack should have, according
+        to the convention used.
+
+        :return: the filename that should be used.
+        :rtype: str
+        """
         if self.metadata.support is None:
             support_suffix = ""
         else:
@@ -46,6 +93,15 @@ class HardwarePack(object):
             self.metadata.name, self.metadata.version, support_suffix)
 
     def to_f(self, fileobj):
+        """Write the hwpack to a file object.
+
+        The full hardware pack will be written to the file object in
+        gzip compressed tarball form as the spec requires.
+
+        :param fileobj: the file object to write to.
+        :type fileobj: a file-like object
+        :return: None
+        """
         kwargs = {}
         kwargs["default_uid"] = 1000
         kwargs["default_gid"] = 1000
