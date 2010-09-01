b59396ba26f5f85bc69654e9fda642fed0ed62e6
James Westby <james.westby@canonical.com>
2010-09-01 17:12:48 -0400

    Add some tests and docstrings.

--- hwpack/packages.py
+++ hwpack/packages.py
@@ -8,9 +8,46 @@ from bzrlib.transport import get_transport
 from bzrlib import urlutils
 
 
+def ensure_file_uri_starts_with_three_slashes(uri):
+    """Ensure that file: URIs are in a format suitable for use with bzrlib.
+
+    apt can handle file:/something URIs with no problem, but bzrlib
+    rejects them. We therefore fix this up ourselves as we know how
+    apt would handle them.
+
+    :param uri: the uri to check.
+    :type uri: str
+    :return: the modified uri.
+    :rtype: str
+    """
+    if uri.startswith("file:/") and uri[len("file:/")] != '/':
+        uri = "file:///" + uri[len("file:/"):]
+    return uri
+
+
 class PackageFetcher(object):
+    """A class to fetch packages from a defined list of sources."""
 
     def __init__(self, sources):
+        """Create a PackageFetcher.
+
+        Once created a PackageFetcher should have its `prepare` method
+        called before use.
+
+        :param sources: a list of sources such that they can be prefixed
+            with "deb " and fed to apt.
+        :type sources: an iterable of str
+        """
+        self.sources = sources
+        self.tempdir = None
+
+    def prepare(self):
+        """Prepare a PackageFetcher for use.
+
+        Should be called before use, and after any modification to the list
+        of sources.
+        """
+        self.cleanup()
         self.tempdir = tempfile.mkdtemp(prefix="hwpack-apt-cache-")
         files = ["var/lib/dpkg/status",
                 ]
@@ -27,16 +64,33 @@ class PackageFetcher(object):
         sources_list = os.path.join(
             self.tempdir, "etc", "apt", "sources.list")
         with open(sources_list, 'w') as f:
-            for source in sources:
+            for source in self.sources:
                 f.write("deb %s\n" % source)
         self.cache = Cache(rootdir=self.tempdir, memonly=True)
         self.cache.update()
         self.cache.open()
 
     def cleanup(self):
-        shutil.rmtree(self.tempdir)
+        """Cleanup any remaining artefacts.
+
+        Should be called on all PackageFetchers when they are finished
+        with.
+        """
+        if self.tempdir is not None and os.path.exists(self.tempdir):
+            shutil.rmtree(self.tempdir)
 
     def fetch_packages(self, packages):
+        """Fetch the files for the given list of package names.
+
+        :param packages: a list of package names to install
+        :type packages: an iterable of str
+        :return: a dict containing the filenames of the .debs that were
+            fetched as the keys, and file objects with the contents of
+            those debs as the values.
+        :rtype: a dict mapping str to file-like objects.
+        :raises KeyError: if any of the package names in the list couldn't
+            be found.
+        """
         results = {}
         possible_transports = []
         for package in packages:
@@ -44,8 +98,7 @@ class PackageFetcher(object):
             candidate = apt_pkg.candidate
             base = os.path.basename(candidate.filename)
             dir_url = urlutils.dirname(candidate.uri)
-            if dir_url.startswith("file:/") and dir_url[len("file:/")] != '/':
-                dir_url = "file:///" + dir_url[len("file:/"):]
+            dir_url = ensure_file_uri_starts_with_three_slashes(dir_url)
             results[base] = get_transport(
                 dir_url,
                 possible_transports=possible_transports).get(base)
--- hwpack/tests/test_packages.py
+++ hwpack/tests/test_packages.py
@@ -5,7 +5,10 @@ import tempfile
 
 from testtools import TestCase
 
-from hwpack.packages import PackageFetcher
+from hwpack.packages import (
+    ensure_file_uri_starts_with_three_slashes,
+    PackageFetcher,
+    )
 
 
 class Package(object):
@@ -56,6 +59,30 @@ class AptSource(object):
         return "file:" + os.path.abspath(self.rootdir) +" ./"
 
 
+class EnsureURITests(TestCase):
+
+    def test_modifies_file_uri_with_one_slash(self):
+        uri = "file:/something"
+        self.assertEqual(
+            "file:///something",
+            ensure_file_uri_starts_with_three_slashes(uri))
+
+    def test_doesnt_modify_file_uri_with_three_slashes(self):
+        uri = "file:///something"
+        self.assertEqual(
+            uri, ensure_file_uri_starts_with_three_slashes(uri))
+
+    def test_doesnt_modify_file_uri_with_hostname(self):
+        uri = "file://localhost/something"
+        self.assertEqual(
+            uri, ensure_file_uri_starts_with_three_slashes(uri))
+
+    def test_doesnt_modify_http_uri(self):
+        uri = "http://example.org/something"
+        self.assertEqual(
+            uri, ensure_file_uri_starts_with_three_slashes(uri))
+
+
 class PackageFetcherTests(TestCase):
 
     def useFixture(self, fixture):
@@ -63,45 +90,45 @@ class PackageFetcherTests(TestCase):
         fixture.setUp()
         return fixture
 
-    def test_fetch_packages_not_found_because_no_sources(self):
-        fetcher = PackageFetcher([])
+    def get_fetcher(self, sources):
+        fetcher = PackageFetcher([s.sources_entry for s in sources])
         self.addCleanup(fetcher.cleanup)
+        fetcher.prepare()
+        return fetcher
+
+    def test_fetch_packages_not_found_because_no_sources(self):
+        fetcher = self.get_fetcher([])
         self.assertRaises(KeyError, fetcher.fetch_packages, ["nothere"])
 
     def test_fetch_packages_not_found_because_not_in_sources(self):
         available_package = Package("foo", "1.0")
         source = self.useFixture(AptSource([available_package]))
-        fetcher = PackageFetcher([source.sources_entry])
-        self.addCleanup(fetcher.cleanup)
+        fetcher = self.get_fetcher([source])
         self.assertRaises(KeyError, fetcher.fetch_packages, ["nothere"])
 
     def test_fetch_packages_not_found_one_of_two_missing(self):
         available_package = Package("foo", "1.0")
         source = self.useFixture(AptSource([available_package]))
-        fetcher = PackageFetcher([source.sources_entry])
-        self.addCleanup(fetcher.cleanup)
+        fetcher = self.get_fetcher([source])
         self.assertRaises(
             KeyError, fetcher.fetch_packages, ["foo", "nothere"])
 
     def test_fetch_packges_fetches_no_packages(self):
         available_package = Package("foo", "1.0")
         source = self.useFixture(AptSource([available_package]))
-        fetcher = PackageFetcher([source.sources_entry])
-        self.addCleanup(fetcher.cleanup)
+        fetcher = self.get_fetcher([source])
         self.assertEqual(0, len(fetcher.fetch_packages([])))
 
     def test_fetch_packges_fetches_single_package(self):
         available_package = Package("foo", "1.0")
         source = self.useFixture(AptSource([available_package]))
-        fetcher = PackageFetcher([source.sources_entry])
-        self.addCleanup(fetcher.cleanup)
+        fetcher = self.get_fetcher([source])
         self.assertEqual(1, len(fetcher.fetch_packages(["foo"])))
 
     def test_fetch_packges_fetches_correct_filename(self):
         available_package = Package("foo", "1.0")
         source = self.useFixture(AptSource([available_package]))
-        fetcher = PackageFetcher([source.sources_entry])
-        self.addCleanup(fetcher.cleanup)
+        fetcher = self.get_fetcher([source])
         self.assertEqual(
             [available_package.filename],
             fetcher.fetch_packages(["foo"]).keys())
@@ -109,8 +136,7 @@ class PackageFetcherTests(TestCase):
     def test_fetch_packges_fetches_correct_contents(self):
         available_package = Package("foo", "1.0")
         source = self.useFixture(AptSource([available_package]))
-        fetcher = PackageFetcher([source.sources_entry])
-        self.addCleanup(fetcher.cleanup)
+        fetcher = self.get_fetcher([source])
         self.assertEqual(
             available_package.content,
             fetcher.fetch_packages(
@@ -119,15 +145,13 @@ class PackageFetcherTests(TestCase):
     def test_fetch_packges_fetches_multiple_packages(self):
         available_packages = [Package("bar", "1.0"), Package("foo", "1.0")]
         source = self.useFixture(AptSource(available_packages))
-        fetcher = PackageFetcher([source.sources_entry])
-        self.addCleanup(fetcher.cleanup)
+        fetcher = self.get_fetcher([source])
         self.assertEqual(2, len(fetcher.fetch_packages(["foo", "bar"])))
 
     def test_fetch_packges_fetches_multiple_packages_correctly(self):
         available_packages = [Package("bar", "1.0"), Package("foo", "1.0")]
         source = self.useFixture(AptSource(available_packages))
-        fetcher = PackageFetcher([source.sources_entry])
-        self.addCleanup(fetcher.cleanup)
+        fetcher = self.get_fetcher([source])
         fetched_contents = fetcher.fetch_packages(["foo", "bar"])
         self.assertEqual(
             dict([(p.filename, p.content) for p in available_packages]),
@@ -137,8 +161,7 @@ class PackageFetcherTests(TestCase):
     def test_fetch_packages_fetches_newest(self):
         available_packages = [Package("bar", "1.0"), Package("bar", "1.1")]
         source = self.useFixture(AptSource(available_packages))
-        fetcher = PackageFetcher([source.sources_entry])
-        self.addCleanup(fetcher.cleanup)
+        fetcher = self.get_fetcher([source])
         fetched_contents = fetcher.fetch_packages(["bar"])
         self.assertEqual(
             {available_packages[1].filename: available_packages[1].content},
@@ -150,9 +173,7 @@ class PackageFetcherTests(TestCase):
         new_source_packages = [Package("bar", "1.1")]
         old_source = self.useFixture(AptSource(old_source_packages))
         new_source = self.useFixture(AptSource(new_source_packages))
-        fetcher = PackageFetcher(
-            [old_source.sources_entry, new_source.sources_entry])
-        self.addCleanup(fetcher.cleanup)
+        fetcher = self.get_fetcher([old_source, new_source])
         fetched_contents = fetcher.fetch_packages(["bar"])
         self.assertEqual(
             {new_source_packages[0].filename: new_source_packages[0].content},
