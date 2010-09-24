c4f045c85e4eb724ed5e297693f2c9319c9c95e2
James Westby <james.westby@canonical.com>
2010-09-24 16:37:48 -0400

    Package the hardware pack tools.

---
+++ setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+import os
+import subprocess
+
+
+def get_version():
+    proc = subprocess.Popen(
+        ["dpkg-parsechangelog"],
+        cwd=os.path.abspath(os.path.dirname(__file__)),
+        stdout=subprocess.PIPE)
+    output, _ = proc.communicate()
+    version = None
+    for line in output.split("\n"):
+        if line.startswith("Version: "):
+            version = line.split(" ", 1)[1].strip()
+    assert version is not None, (
+        "Couldn't determine version number from debian changelog")
+
+
+setup(
+        name="hwpack",
+        version=get_version(),
+        packages=["hwpack"],
+     )
