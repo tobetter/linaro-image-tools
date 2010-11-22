18365f836dafba1b0666d437b041ee58f53079cd
Guilherme Salgado <salgado@canonical.com>
2010-11-22 11:17:27 -0200

    Add instructions for running the tests and create a new top-level test_suite, used to run the whole test suite with subunit

---
+++ tests/__init__.py
@@ -0,0 +1,8 @@
+from hwpack.tests import test_suite as hwpack_suite
+from media_create.tests import test_suite as media_create_suite
+
+
+def test_suite():
+    suite = hwpack_suite()
+    suite.addTests(media_create_suite())
+    return suite
