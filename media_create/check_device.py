b80bcee931a8963cd7c3566e596603ceb47d6b33
Martin Ohlsson <martin.ohlson@linaro.org>
2010-12-14 15:32:45 +0100

    Refactor for PEP-8 compliance.

--- media_create/check_device.py
+++ media_create/check_device.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 import os
 import re
 import sys
@@ -12,6 +10,7 @@ def _run_proc(args, last_arg, env, stdin=None):
         return p.communicate()[0]
     else:
         return p.stdout
+
 
 def _exec_fdisk_and_grep(as_root, env, grep_str, extra_cmd=None):
     args = []
@@ -27,13 +26,16 @@ def _exec_fdisk_and_grep(as_root, env, grep_str, extra_cmd=None):
             stdin=pipe)
         return _run_proc(extra_cmd, True, env, stdin=pipe)
 
+
 def _find_device(as_root, device, env):
     return _exec_fdisk_and_grep(as_root, env, '\"Disk %s\"' % device,
         extra_cmd=['awk', '{print $2}'])
 
+
 def _print_fdisk_dev(as_root, env):
     sys.stdout.write('\nfdisk -l:\n')
     _exec_fdisk_and_grep(as_root, env, '\"Disk /dev/\"')
+
 
 def _print_mounted_devices():
     mount_file = '/etc/mtab'
@@ -46,14 +48,17 @@ def _print_mounted_devices():
     except IOError:
         sys.stdout.write('IOError: cannot read %s\n' % mount_file)
 
+
 def _select_device(device):
     resp = raw_input('Are you 100% sure, on selecting [%s] (y/n)? ' % device)
     if resp.lower() != 'y':
         return False
     return True
 
+
 def _get_extended_env(ext):
     return os.environ.update(ext)
+
 
 def check_device(device, as_root=True):
     """ Checks that a selected device exists.
@@ -77,6 +82,7 @@ def check_device(device, as_root=True):
         _print_mounted_devices()
 
     return 1
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
