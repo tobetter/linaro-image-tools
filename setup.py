1d4fed29df5f2c915f241d081746257ca93fa9fa
Loïc Minier <lool@dooz.org>
2011-01-28 13:17:42 +0100

    Add misc metadata: description, license, URL, author information...

--- setup.py
+++ setup.py
@@ -7,6 +7,12 @@ import subprocess
 setup(
         name="linaro-image-tools",
         version="0.4.1.1",
+        description="Tools to create and write Linaro images",
+        url="https://launchpad.net/linaro-image-tools",
+        license="GPL v2 or later",
+        author='Linaro Infrastructure team',
+        author_email="linaro-dev@lists.linaro.org",
+
         packages=["hwpack", "linaro_media_create"],
         scripts=[
             "linaro-hwpack-create", "linaro-hwpack-install",
