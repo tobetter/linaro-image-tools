d5168b264e32ba01e271f3b7ebad2861671a2af6
Loïc Minier <lool@dooz.org>
2011-01-28 13:07:05 +0100

    Harcode version in setup.py instead of reading it from the packaging which will go away.  Use version 0.4.1.1 as to distinguish people using bzr checkouts from the 0.4.1 release.

--- setup.py
+++ setup.py
@@ -4,23 +4,8 @@ from distutils.core import setup
 import os
 import subprocess
 
-
-def get_version():
-    proc = subprocess.Popen(
-        ["dpkg-parsechangelog"],
-        cwd=os.path.abspath(os.path.dirname(__file__)),
-        stdout=subprocess.PIPE)
-    output, _ = proc.communicate()
-    version = None
-    for line in output.split("\n"):
-        if line.startswith("Version: "):
-            version = line.split(" ", 1)[1].strip()
-    assert version is not None, (
-        "Couldn't determine version number from debian changelog")
-
-
 setup(
         name="linaro-image-tools",
-        version=get_version(),
+        version="0.4.1.1",
         packages=["hwpack", "linaro_media_create"],
      )
