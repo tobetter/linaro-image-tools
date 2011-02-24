9eba1f5e222f4b306009eb973f406580ac79497d
Loïc Minier <lool@dooz.org>
2011-02-24 20:36:23 +0100

    Drop unused errno import.

--- linaro_media_create/populate_boot.py
+++ linaro_media_create/populate_boot.py
@@ -17,7 +17,6 @@
 # You should have received a copy of the GNU General Public License
 # along with Linaro Image Tools.  If not, see <http://www.gnu.org/licenses/>.
 
-import errno
 import os
 
 from linaro_media_create import cmd_runner
