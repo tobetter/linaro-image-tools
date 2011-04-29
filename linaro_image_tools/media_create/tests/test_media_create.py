8876dc684a22221e14360960823e2ff7c1505e65
Loïc Minier <loic.minier@linaro.org>
2011-04-29 12:52:43 +0200

    Use dd instead of qemu-img to create sparse files.

--- linaro_image_tools/media_create/tests/test_media_create.py
+++ linaro_image_tools/media_create/tests/test_media_create.py
@@ -981,7 +981,7 @@ class TestPartitionSetup(TestCaseWithFixtures):
             'root', 'ext3', True, True, True)
         self.assertEqual(
              # This is the call that would create a 2 GiB image file.
-            ['qemu-img create -f raw %s 2147483648' % tmpfile,
+            ['dd of=%s bs=1 seek=2147483648' % tmpfile,
              # This call would partition the image file.
              '%s sfdisk --force -D -uS -H %s -S %s -C 1024 %s' % (
                  sudo_args, HEADS, SECTORS, tmpfile),
