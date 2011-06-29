851f277a0de31a82f6243b2275498aa6eec75e44
James Westby <james.westby@linaro.org>
2011-06-29 15:44:15 -0400

    Post-release version bump to 2011.06-1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import DistUtilsExtra.auto
 
 DistUtilsExtra.auto.setup(
         name="linaro-image-tools",
-        version="2011.06-1",
+        version="2011.06-1.1",
         description="Tools to create and write Linaro images",
         url="https://launchpad.net/linaro-image-tools",
         license="GPL v3 or later",
