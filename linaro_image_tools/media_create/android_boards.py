5c21d79052f740b746d466b67fa17a771c1ae0b4
Angus Ainslie <angus.ainslie@linaro.org>
2011-07-27 19:15:14 -0600

    Fix botched merge

--- linaro_image_tools/media_create/android_boards.py
+++ linaro_image_tools/media_create/android_boards.py
@@ -230,13 +230,15 @@ class AndroidMx53LoCoConfig(AndroidBoardConfig, Mx53LoCoConfig):
 
     @classmethod
     def get_sfdisk_cmd(cls, should_align_boot_part=False):
-    
-	loader_start, loader_end, loader_len = align_partition(
+        loader_start, loader_end, loader_len = align_partition(
             1, cls.LOADER_MIN_SIZE_S, 1, PART_ALIGN_S)
-        
+
 	command = super(AndroidMx53LoCoConfig, cls).get_sfdisk_cmd(
             should_align_boot_part=True, start_addr=loader_end,
             extra_part=True)
+
+        return '%s,%s,0xDA\n%s' % (
+            loader_start, loader_len, command)
 
     @classmethod
     def install_boot_loader(cls, boot_partition, boot_device_or_file):
