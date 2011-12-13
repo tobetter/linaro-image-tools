f46b62dba27ff883c3dbac1b25b67dee6dc30fc2
Georgy Redkozubov <georgy.redkozubov@linaro.org>
2011-12-13 16:36:05 +0400

    Proposed fix. Wait in loop for disk being freed and run sfdisk after that.

--- linaro_image_tools/media_create/partitions.py
+++ linaro_image_tools/media_create/partitions.py
@@ -42,6 +42,9 @@ SECTOR_SIZE = 512 # bytes
 CYLINDER_SIZE = HEADS * SECTORS * SECTOR_SIZE
 DBUS_PROPERTIES = 'org.freedesktop.DBus.Properties'
 UDISKS = "org.freedesktop.UDisks"
+# Max number of attempts to sleep (total sleep time in seconds =
+# 1+2+...+MAX_TTS)
+MAX_TTS = 10
 
 
 def setup_android_partitions(board_config, media, image_size, bootfs_label,
@@ -516,6 +519,8 @@ def create_partitions(board_config, media, heads, sectors, cylinders=None,
             ['parted', '-s', media.path, 'mklabel', 'msdos'], as_root=True)
         proc.wait()
 
+    wait_partition_to_settle(media)
+
     sfdisk_cmd = board_config.get_sfdisk_cmd(
         should_align_boot_part=should_align_boot_part)
 
@@ -523,11 +528,28 @@ def create_partitions(board_config, media, heads, sectors, cylinders=None,
 
     # Sync and sleep to wait for the partition to settle.
     cmd_runner.run(['sync']).wait()
-    # Sleeping just 1 second seems to be enough here, but if we start getting
-    # errors because the disk is not partitioned then we should revisit this.
-    # XXX: This sleep can probably die now; need to do more tests before doing
-    # so, though.
-    time.sleep(1)
+    wait_partition_to_settle(media)
+
+
+def wait_partition_to_settle(media):
+    """Sleep in a loop to wait partition to settle
+
+    :param media: A setup_partitions.Media object to partition.
+    """
+    tts = 1
+    while (tts > 0) and (tts <= MAX_TTS):
+        try:
+            print "Sleeping for %s second(s) to wait for the partition to settle" % tts
+            time.sleep(tts)
+            proc = cmd_runner.run(['sfdisk', '-l', media.path], as_root=True, stdout=open('/dev/null', 'w'))
+            proc.wait()
+            tts = 0
+        except cmd_runner.SubcommandNonZeroReturnValue:
+            print "Partition table is not available."
+            tts += 1
+    if tts:
+        print "Error: Couldn't read partition table for a reasonable time."
+        raise
 
 
 class Media(object):
