4cf6eb593a76396c7d8454d508721de99a228b93
Milo Casagrande <milo@ubuntu.com>
2012-06-06 12:02:34 +0200

    Fixed test case with new method needing output.

--- linaro_image_tools/tests/test_utils.py
+++ linaro_image_tools/tests/test_utils.py
@@ -24,6 +24,7 @@ import sys
 import logging
 import tempfile
 import tarfile
+from StringIO import StringIO
 
 from linaro_image_tools import cmd_runner, utils
 from linaro_image_tools.testing import TestCaseWithFixtures
@@ -248,14 +249,21 @@ class TestFindCommand(TestCaseWithFixtures):
 class TestInstallPackageProviding(TestCaseWithFixtures):
 
     def test_found_package(self):
-        self.useFixture(MockSomethingFixture(
-            sys, 'stdout', open('/dev/null', 'w')))
-        fixture = self.useFixture(MockCmdRunnerPopenFixture())
-        install_package_providing('mkfs.vfat')
-        self.assertEqual(
-            ['apt-get -s install dosfstools',
-             '%s apt-get --yes install dosfstools' % sudo_args],
-            fixture.mock.commands_executed)
+        try:
+            output = StringIO()
+            self.useFixture(MockSomethingFixture(
+                                                 sys,
+                                                 'stdout',
+                                                 open(output, 'w')))
+            fixture = self.useFixture(MockCmdRunnerPopenFixture())
+            install_package_providing('mkfs.vfat')
+            self.assertEqual(
+                             ['apt-get -s install dosfstools',
+                              '%s apt-get --yes install dosfstools' %
+                              sudo_args],
+                             fixture.mock.commands_executed)
+        finally:
+            output.close()
 
     def test_not_found_package(self):
         self.assertRaises(
