8e13ee2df58453a8d6f387cea946e9da18f4615c
Milo Casagrande <milo@ubuntu.com>
2012-07-09 14:35:16 +0200

    Added first version of the converstion tool.

---
+++ linaro_image_tools/hwpack/hwpack_convert.py
@@ -0,0 +1,111 @@
+import ConfigParser
+import logging
+import os
+import os.path
+
+# This is the main section of an INI-style hwpack config file.
+MAIN_SECTION = 'hwpack'
+# This, if has multiple values, should be converted into the proper structure.
+ARCHITECTURES_KEY = 'architectures'
+# The suffix for the new file
+NEW_FILE_SUFFIX = '.yaml'
+
+logger = logging.getLogger("linaro_hwpack_converter")
+
+
+class HwpackConverterException(Exception):
+    """General exception class for the converter."""
+
+
+class HwpackConverter(object):
+    """Simple and basic class that converts an INI-style format file into the
+    new YAML format.
+
+    The old format number is maintained.
+
+    :param input_file: the input file to parse, has to be an INI-style file.
+    :param output_file: where to write the new file, if not given, the name
+                        of the input file will be used adding the 'yaml'
+                        suffix.
+    """
+    def __init__(self, input_file, output_file):
+        """Initializie the class."""
+        self.input_file = input_file
+        self.output_file = output_file
+
+        self.parser = ConfigParser.RawConfigParser()
+        # Where we store the list of sources.
+        self.sources = {}
+        # Where we store all the information of the hwpack config file
+        # In this case we have one board per hwpack config file.
+        self.hwpack = {}
+
+    def parse(self):
+        """Parses the config file and stores its values."""
+        with open(self.input_file, 'r') as fp:
+            self.parser.readfp(fp)
+
+        # Iterate through all the file sections.
+        for section in self.parser.sections():
+            if section == MAIN_SECTION:
+                for key, value in self.parser.items(section):
+                    if value is not None:
+                        if value == ARCHITECTURES_KEY:
+                            pass
+                        self.hwpack[key] = value
+            else:
+                for _, value in self.parser.items(section):
+                    if value is not None:
+                        self.sources[section] = value
+
+    def _to_file(self):
+        """Writes the converted hwpack to file."""
+        with open(self.output_file, 'w') as fp:
+            fp.write(str(self))
+
+    def convert(self):
+        """Converts the input file into the output file with the new format.
+        """
+        self._parse()
+        self._to_file()
+
+    def __str__(self):
+        """Readable representation of the converted hwpack."""
+        converted = ''
+        if self.hwpack:
+            for key, value in self.hwpack.iteritems():
+                # Convert 'Yes' and 'No' values into boolean.
+                if value == 'Yes':
+                    value = True
+                elif value == 'No':
+                    value = False
+                converted += "%s: %s\n" % (key, value)
+        if self.sources:
+            converted += "sources:\n"
+            for key, value in self.sources.iteritems():
+                # Keep the heading white-space, we need it to define nesting.
+                converted += " %s: %s\n" % (key, value)
+        return converted
+
+
+def check_and_validate_args(args):
+    """Assures that the args passed are valid.
+
+    :param args: the args as defined in linaro-hwpack-convert.
+    """
+    input_file = args.CONFIG_FILE
+    output_file = args.out
+    if not os.path.exists(input_file) or not os.path.isfile(input_file):
+        raise HwpackConverterException("The configuration file '%s' is not a "
+                                        "regular file." % input_file)
+    if output_file is not None:
+        if os.path.exists(output_file) or os.path.isdir(output_file):
+            raise HwpackConverterException("The output file name provided "
+                                            "'%s' already exists, or is a "
+                                            "directory." % output_file)
+        elif not os.path.isabs(output_file):
+            # If we output file is just a name, write it in the current dir.
+            output_file = os.path.join(os.getcwd(), output_file)
+    else:
+        output_file = input_file + NEW_FILE_SUFFIX
+    return (input_file, output_file)
---
+++ linaro_image_tools/hwpack/tests/test_hwpack_converter.py
@@ -0,0 +1,62 @@
+#from StringIO import StringIO
+import tempfile
+#import os
+#import os.path
+#from testtools.matchers import Equals, MismatchError
+from linaro_image_tools.testing import TestCaseWithFixtures
+from linaro_image_tools.tests.fixtures import (
+    CreateTempDirFixture,
+    CreateTempFileFixture,
+#    MockCmdRunnerPopenFixture,
+#    MockSomethingFixture,
+    )
+
+from linaro_image_tools.hwpack.hwpack_convert import (
+    HwpackConverter,
+    HwpackConverterException,
+    check_and_validate_args,
+    )
+
+
+class Args():
+    """Defines the args for the command line options."""
+    def __init__(self, input_file, output_file=None):
+        self.CONFIG_FILE = input_file
+        self.out = output_file
+
+
+class HwpackConverterTests(TestCaseWithFixtures):
+    """Test class for the hwpack converter."""
+
+    def setUp(self):
+        super(HwpackConverterTests, self).setUp()
+
+    def test_wrong_input_file(self):
+        """Pass a non-existing file."""
+        input_file = '/tmp/foobaz'
+        self.assertRaises(HwpackConverterException, check_and_validate_args,
+                            Args(input_file=input_file))
+
+    def test_wrong_input_dir(self):
+        """Pass a directory instead of file."""
+        temp_file = tempfile.NamedTemporaryFile()
+        temp_dir = self.useFixture(CreateTempDirFixture()).get_temp_dir()
+        self.assertRaises(HwpackConverterException, check_and_validate_args,
+                            Args(input_file=temp_file.name,
+                                output_file=temp_dir))
+
+    def test_same_input_output_file(self):
+        """Pass the same existing file path to the two arguments."""
+        temp_file = self.useFixture(CreateTempFileFixture()).get_file_name()
+        self.assertRaises(HwpackConverterException, check_and_validate_args,
+                            Args(input_file=temp_file, output_file=temp_file))
+
+    def test_basic_parse(self):
+        ini_format = '[hwpack]\nformat=2.0\nsupport=supported'
+        output_format = 'support: supported\nformat: 2.0\n'
+        input_file = self.useFixture(CreateTempFileFixture(ini_format)).\
+                                                                get_file_name()
+        output_file = self.useFixture(CreateTempFileFixture()).get_file_name()
+        converter = HwpackConverter(input_file, output_file)
+        converter.parse()
+        self.assertEqual(output_format, str(converter))
